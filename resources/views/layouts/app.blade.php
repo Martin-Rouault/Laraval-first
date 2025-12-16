9445e28512d095894199a9bc312a2543e704fc22
Martin-Rouault <martin.rlt24@gmail.com>
2025-12-16 11:46:07 +0100

    changed html lang to dynamic

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
     <meta charset="UTF-8">
@@ -7,9 +7,7 @@
 </head>
 
 <body>
-    <header>
-        <h1>Mon site Laravel</h1>
-    </header>
+    @include('partials.header')
     <main>
         @yield('content')
     </main>
