153d5d55c012cc277d4db1cf499eb85ea71dba8e
Martin-Rouault <martin.rlt24@gmail.com>
2025-12-16 17:09:37 +0100

    add app.css & app.js to layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<link rel="icon" type="image/x-icon" href="/favicon.png">
+<link rel="icon" type="image/x-icon" href="{{ asset('/favicon.png') }}">
 
 <head>
     <meta charset="UTF-8">
     <title>@yield('title')</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body>
